6641443b6450b35e39bc2ecfb69ca49c8e7a805d
Mel Chua <mchuam@gmail.com>
2024-07-23 17:26:16 +0700

    Filled in chart with real data

--- app/components/LineChart.tsx
+++ app/components/LineChart.tsx
@@ -1,87 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as d3 from "d3";
+import styles from "./LineChart.module.css";
 
-const dummyQuotes = [
-  {
-    date: "2021-05-08T00:00:00.000Z",
-    high: 59464.61328125,
-    volume: 65382980634,
-    open: 57352.765625,
-    low: 56975.2109375,
-    close: 58803.77734375,
-    adjclose: 58803.77734375,
-  },
-  {
-    date: "2021-05-09T00:00:00.000Z",
-    high: 59210.8828125,
-    volume: 65906690347,
-    open: 58877.390625,
-    low: 56482.00390625,
-    close: 58232.31640625,
-    adjclose: 58232.31640625,
-  },
-  {
-    date: "2021-05-10T00:00:00.000Z",
-    high: 59519.35546875,
-    volume: 71776546298,
-    open: 58250.87109375,
-    low: 54071.45703125,
-    close: 55859.796875,
-    adjclose: 55859.796875,
-  },
-  {
-    date: "2021-05-11T00:00:00.000Z",
-    high: 56872.54296875,
-    volume: 61308396325,
-    open: 55847.2421875,
-    low: 54608.65234375,
-    close: 56704.57421875,
-    adjclose: 56704.57421875,
-  },
-  {
-    date: "2021-05-12T00:00:00.000Z",
-    high: 57939.36328125,
-    volume: 75215403907,
-    open: 56714.53125,
-    low: 49150.53515625,
-    close: 49150.53515625,
-    adjclose: 49150.53515625,
-  },
-  {
-    date: "2021-05-13T00:00:00.000Z",
-    high: 51330.84375,
-    volume: 96721152926,
-    open: 49735.43359375,
-    low: 46980.01953125,
-    close: 49716.19140625,
-    adjclose: 49716.19140625,
-  },
-  {
-    date: "2021-05-14T00:00:00.000Z",
-    high: 51438.1171875,
-    volume: 55737497453,
-    open: 49682.98046875,
-    low: 48868.578125,
-    close: 49880.53515625,
-    adjclose: 49880.53515625,
-  },
-  {
-    date: "2021-05-15T00:00:00.000Z",
-    high: 50639.6640625,
-    volume: 59161047474,
-    open: 49855.49609375,
-    low: 46664.140625,
-    close: 46760.1875,
-    adjclose: 46760.1875,
-  },
-  {
-    date: "2021-05-16T00:00:00.000Z",
-    close: 55859.796875,
-    adjclose: 55859.796875,
-  },
-];
-
-export default function () {
+export default function ({ data }) {
   const d3Container = useRef(null);
 
   const drawChart = () => {
@@ -93,10 +14,7 @@ export default function () {
     const marginBottom = 30;
     const marginLeft = 40;
 
-    const xExtent = d3.extent(dummyQuotes, (d) => new Date(d.date)) as [
-      Date,
-      Date
-    ];
+    const xExtent = d3.extent(data, (d) => new Date(d.date)) as [Date, Date];
     const x = d3.scaleUtc(xExtent || [new Date(), new Date()], [
       marginLeft,
       width - marginRight,
@@ -104,12 +22,17 @@ export default function () {
 
     // Declare the y (vertical position) scale.
     const y = d3.scaleLinear(
-      [
-        d3.min(dummyQuotes, (d) => d.close) || 0,
-        d3.max(dummyQuotes, (d) => d.close) || 0,
-      ],
+      [d3.min(data, (d) => d.close) || 0, d3.max(data, (d) => d.close) || 0],
       [height - marginBottom, marginTop]
     );
+
+    var area = d3
+      .area()
+      .x((p) => {
+        return x(new Date(p.date));
+      })
+      .y0((p) => height - marginBottom)
+      .y1((p) => y(p.close));
 
     // Declare the line generator.
     const line = d3
@@ -132,7 +55,7 @@ export default function () {
       .call(
         d3
           .axisBottom(x)
-          .ticks(width / 200)
+          .ticks(width / 80)
           .tickSizeOuter(0)
       );
 
@@ -159,13 +82,20 @@ export default function () {
           .text("↑ Daily close ($)")
       );
 
-    // Append a path for the line.
+    // fill it
     svg
       .append("path")
-      .attr("fill", "none")
+      .attr("fill", "lightsteelblue")
       .attr("stroke", "steelblue")
-      .attr("stroke-width", 3)
-      .attr("d", line(dummyQuotes));
+      .attr("d", area(data));
+
+    // Append a path for the line.
+    // svg
+    //   .append("path")
+    //   .attr("fill", "none")
+    //   .attr("stroke", "steelblue")
+    //   .attr("stroke-width", 1)
+    //   .attr("d", line(data));
 
     // // Bind D3 data
     // const update = svg.append("g").selectAll("text").data(data);
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,33 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 import LineChart from "../components/LineChart";
+import yahooFinance from "yahoo-finance2";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Bitcoin Charter" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+export const loader = async () => {
+  const query = "BTC-USD";
+  const queryOptions = { period1: "2021-05-08" /* ... */ };
+  const result = await yahooFinance.chart(query, queryOptions);
+
+  return json(result);
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
+  console.log("data", data);
+  const quotes = data?.quotes;
   return (
-    <div className="font-sans p-4">
+    <div className="font-mono p-4">
       <h1>BIT-COIN USD</h1>
-      <LineChart />
+      <LineChart data={quotes} />
     </div>
   );
 }
